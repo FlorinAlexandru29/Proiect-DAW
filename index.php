71b2987c5ca91cb2c13f2bfaf25481bca34926a0
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2022-12-05 18:43:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,53 +8,5 @@ echo getcwd();
 
 
 ?>
-<HEAD>
-<Title> Home </Title>
-<link rel="stylesheet" type="text/css" href="CSS/poze.css">
-<link rel="stylesheet" type="text/css" href="CSS/meniu.css">
-<link rel="stylesheet" type="text/css" href="CSS/home.css">
-<style>
-@-webkit-keyframes mymove {
-  50% {background-color: #00BFFF;}}
-@keyframes mymove {
-  50% {background-color: #00BFFF;}}
-  @-webkit-keyframes invers {
-  50% {background-color: blue;}}
-@keyframes invers {
-  50% {background-color: blue;}}
- </style>
- <style>
-	</style>
-   </HEAD>
-<BODY background="poze/peisajmain.png">
-<form action="http://localhost/search.php" method="post">
-<div class="search"> <p style="color:red;font-size:1.1vw;padding:0px;font-family:Forte;" class="specie"> Cautare Rapida: </p>
-<input type="text" name="a" style="width:80%;" maxlength="20">
- <input type="image" value="submit" style="display: inline-block;
-	vertical-align: middle;" width="15%" height="30%" src="poze/search.png">  </div> </form> 
-<aside style="background:blue;" class="vertical-menu">
-<font style="background:#00BFFF;" class="active"> MENIU </font>
-  <a href="peisaje.php">Peisaje</a>
-  <a href="Natura.php">Natura</a>
-  <a href="Septicflesh.php">SepticFlesh</a>
-  <a href="Violet.php">VioletCold</a>
-  </aside>
-  <img class="meniu"  src="poze/pozameniu3.jpg">
-  <div  class="iframe-container" style="padding-top:0%;">
-  <iframe class="pag" src="NaturaShow.html"> </iframe>
-  <iframe class="pag" src="SepticShow.html"> </iframe>
-  </div>
-  <div class="descriere">
-  <img class="descriere" src="poze/profil1.jpg"> <br>
-  <p style="height:35%;" class="descriere" > Ma numesc Anghelescu Florin Alexandru, am 18 ani si sunt student in Bucuresti. <BR>
-  Sunt pasionat de fotografie si muzica, doua dintre cele mai nobile arte.<BR>
-  Acest site a fost creat pentru oamenii interesati de aceste doua domenii, 
-  aici vei gasi pozele mele si review-uri la diferite albume(recente si old school) </p>
-  <p class="descriere"  style="padding-top:10%;text-align:center;">
-   Contact </p>
-  <a  class="icon" href="https://www.facebook.com/florinalexandru29"> <img style="height:5%;width:25%;left:15%;" class="icon" src="poze/facebook.png" align="left"> </a>
-  <a class="icon" href="https://www.instagram.com/allexflorin/"> <img style="right:15%;" class="icon" src="poze/insta.png" align="right"> </a>
-  </div>
-  </body>
-  </html>
+
   
