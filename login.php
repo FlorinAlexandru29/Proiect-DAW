6236b81aa5792357a4ecd8fc44bdab3da9af18e9
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2022-12-06 22:10:07 +0200

    d

--- login.php
+++ login.php
@@ -1,9 +1,9 @@
 <?php
 
 
-     setcookie("user_name", "guest", time()+ 360,'/'); // expires after 60 seconds
+     setcookie("user_name", "guest", time()+ 60,'/'); // expires after 60 seconds
 
-     setcookie("user_name2", "guest", time()+ 360,'/'); // expires after 60 seconds 
+     setcookie("user_name2", "guest", time()+ 60,'/'); // expires after 60 seconds 
 
      print_r($_COOKIE);    //output the contents of the cookie array variable 
 ?>
