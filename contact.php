0eb17539a1785c4b466bfe1021b8a210ec161208
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2022-12-06 17:13:29 +0200

    d

--- contact.php
+++ contact.php
@@ -1,5 +1,48 @@
-<?php 
-include 'resurse/mailer.php';
+<?php
+
+
+require_once('resurse/phpmailer/class.phpmailer.php');
+
+
+$mail = new PHPMailer(true); 
+
+$mail->IsSMTP();
+
+$mail->SMTPDebug = 2;
+
+
+    $name=$_POST['nume'];
+    $email=$_POST['email'];
+    $telephone=$_POST['telefon'];
+    $message=$_POST['mesaj'];
+
+    if(isset($_POST['submit'])){
+try {
+ 
+        $mail->SMTPAuth   = true; 
+        $mail->SMTPSecure = "ssl";                 
+        $mail->Host       = "smtp.gmail.com";    
+        $mail->Port       = 465;                  
+        $mail->Username   = 'lure.production@gmail.com'; 			// GMAIL username
+        $mail->Password   = 'lvupjjdmckeunbal';           // GMAIL password
+        $mail->AddReplyTo('lure.production@gmail.com', 'Lure Prod');
+        $mail->AddAddress('lure.production@gmail.com', 'Lure Prod');
+
+        $mail->isHTML(true);
+        $mail->Subject = 'Contact:' .$name;
+        $mail->Body = "Nume: $name <br> Email: $email <br> Telefon: $telephone <br> Mesaj: $message";
+
+        $mail->send();
+        $alert="<div class='alert-success'><span>Mesaj Trimis</span></div>"; //folosit pentru a afisa mesaj de confirmare, se poate folosi bootstrap
+  echo "Message Sent OK</p>\n";
+
+}
+
+
+  catch(Exception $e){
+    $alert="<div class='alert-error'><span>Eroare Trimitere!'.$e->getMessage().'</span></div>"; 
+}
+}
 
 ?>
 
