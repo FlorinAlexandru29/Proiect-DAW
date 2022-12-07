8c35622397fb3487fde3ed8d94b5d1fa90fb795e
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2022-12-07 23:44:45 +0200

    d

--- Plugin_Tests/lab1-editat.php
+++ Plugin_Tests/lab1-editat.php
@@ -1,45 +1,26 @@
-<html>
-<title> Laboratorul 1 - Forms </title>
-<body>
 <?php
-print_r($_POST);
-if(isset($_POST['send'])){
+
+$conditie="select email FROM users where email='".$_POST['email']." ' ";
+echo $conditie;
+
 $conexiune=mysqli_connect('eu-cdbr-west-03.cleardb.net','bbd126d58cad2b','90feddf5','heroku_45e2f697954b823');
+
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   exit();
 }
 
-$cerere="Insert into users(last_name,first_name,email,password) values ('".$_POST['nume']. "','".$_POST['prenume']. "','" .$_POST['email'] ."','".crypt($_POST['parola'],'sadlsadl')." ')";
-mysqli_query($conexiune, $cerere);
+$result = mysqli_query($conexiune, $cerere);
+echo $result;
 mysqli_close($conexiune);
-echo $cerere;
-}
+
 ?>
-<FORM method="POST" action="lab1-editat.php">
-<table border=0 width="40%" align="left">
-  <tr>
-    <td with="30%">Nume* :</td>
-    <td with="70%"><INPUT TYPE="text" name="nume" required></td>
-  </tr>
-   <tr>
-    <td>Prenume* :</td>
-    <td><INPUT TYPE="text" name="prenume"></td>
-  </tr>
-  <tr>
-    <td>Email* :</td>
-    <td><INPUT TYPE="email" name="email"></td>
-  </tr>
-  </tr>
-    <tr>
-    <td>Parola:</td>
-    <td><INPUT TYPE="password" name="parola"></td>
-  </tr>
-  <tr>
-    <td><INPUT TYPE="reset" VALUE="reset"></td>
-    <td><INPUT TYPE="submit" NAME="send" VALUE="send"></td>
-  </tr>
- </table>
+
+<html>
+  <body>
+  <FORM method="POST" action="lab1-editat.php">
+  Email* :
+    <INPUT TYPE="email" name="email"><INPUT TYPE="submit" name="submit" VALUE="send">
  </form>
-</body>
+ </body>
 </html>
