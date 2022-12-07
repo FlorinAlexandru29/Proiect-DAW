0ed4c1837813f55e80e5f4c2e5beb7accd341736
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2022-12-07 23:39:47 +0200

    d

--- creare_cont.php
+++ creare_cont.php
@@ -6,15 +6,28 @@ echo $_POST['email'];
 $conditie="select email FROM users where email='".$_POST['email']." ' ";
 echo $conditie;
 
-if(isset($_POST['submit'])){
 
-    $conexiune=mysqli_connect('eu-cdbr-west-03.cleardb.net','bbd126d58cad2b','90feddf5','heroku_45e2f697954b823');
+$conexiune=mysqli_connect('eu-cdbr-west-03.cleardb.net','bbd126d58cad2b','90feddf5','heroku_45e2f697954b823');
+
+$result = $connexiune->query($conditie);
+
+echo $result;
+
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
     exit();
   }
 
+if(isset($_POST['submit'])){
+
+    
+
+
 if (($_POST['email'])==("select email FROM users where email='".$_POST['email']." ' ")) echo "Contul este deja creat" ;
+
+
+
+
 else {
 $cerere="Insert into users(last_name,first_name,email,password) values ('".$_POST['nume']. "','".$_POST['prenume']. "','" .$_POST['email'] ."','".crypt($_POST['parola'],'sadlsadl')." ')";
 mysqli_query($conexiune, $cerere);
