ec9a4db6a693b4d55b3fa39f795900e5c054e1d3
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2022-12-07 22:18:00 +0200

    d

--- index.php
+++ index.php
@@ -62,7 +62,10 @@ else {
     <td with="70%"><INPUT TYPE="password" name="password" required></td>
   </tr>
   <td><INPUT TYPE="submit" name="login" VALUE="login"></td>
-  <td> <button type="button" action='creare_cont.php'> Creaza Cont </button> </td>
+  
+  <FORM method='POST' action='creare_cont.php'>
+  <INPUT TYPE='submit' name='creare' VALUE='Creaza Cont'>
+  </form>
   </table>
 </form>
   </body>
