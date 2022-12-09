bb8ec730ca35c7866b29cadd55e09cd3151d82de
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2022-12-10 01:31:34 +0200

    d

--- login.php
+++ login.php
@@ -1,10 +1,49 @@
 <?php
 
 
-     setcookie("user_name", "guest", time()+ 60,'/'); // expires after 60 seconds
+if(isset($_POST['submit'])){
+$conditie_email="select email FROM users where email='".$_POST['email']." ' ";
+$conditie_password="select email FROM users where password='".openssl_encrypt($_POST['parola'], 'AES-128-CTR', 'kalpsdnj', 0, '1234567891011121')." ' ";
+echo $conditie_email;
+echo $conditie_password;
 
-     setcookie("user_name2", "guest", time()+ 60,'/'); // expires after 60 seconds 
+$conexiune=mysqli_connect('eu-cdbr-west-03.cleardb.net','bbd126d58cad2b','90feddf5','heroku_45e2f697954b823');
 
-     print_r($_COOKIE);    //output the contents of the cookie array variable 
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit();
+}
+
+$result_email = mysqli_query($conexiune, $conditie_email);
+$result_password = mysqli_query($conexiune, $conditie_password);
+if ((mysqli_num_rows($result_email) > 0) && (mysqli_num_rows($result_password) > 0))  {
+  echo "Login Realizat cu Succes";
+  $cerere_user="SELECT user_name FROM users WHERE email='".$_POST['email']." ' ";
+     $result_user= mysqli_query($conexiune, $cerere_user);
+  $row = mysqli_fetch_assoc($result_user);
+  echo $row['username'];
+
+  setcookie("user_name", $_POST['email'], time()+ 60,'/');
+} else {
+  echo "Nu a fost gasit contul";
+}
+
+
+
+mysqli_close($conexiune);
+}
 ?>
+
+<html>
+<title> Creare Cont </title>
+  <body>
+  <FORM method="POST" action="login.php">
+    Email* :
+    <INPUT TYPE="email" name="email" required> <br>
+    Parola* :
+    <INPUT TYPE="password" name="parola" required>
+    <INPUT TYPE="submit" name="submit" VALUE="send">
+ </form>
+ </body>
+</html>
 
