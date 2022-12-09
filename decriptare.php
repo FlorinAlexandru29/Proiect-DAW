858e049fb84a2e44f3050ca9e1c217b29d8e8637
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2022-12-10 01:04:12 +0200

    d

--- decriptare.php
+++ decriptare.php
@@ -7,26 +7,25 @@ if(isset($_POST['submit'])){
   $encryption_iv = '1234567891011121';
   $iv_length = openssl_cipher_iv_length($ciphering);
   $options = 0;
-  $encryption_key = $_POST['parola'];
+  if(isset($_POST['parola'])){
 
-  $encryption = openssl_encrypt($_POST['parola'], $ciphering,
-  "kalpsdnj", 0, $encryption_iv);
+  $encryption = openssl_encrypt($_POST['parola'], $ciphering, "kalpsdnj", 0, $encryption_iv);
 
-echo $encryption;
+echo $encryption;}
 
+if(isset($_POST['parola_criptata'])){
 
-
-//$decryption=openssl_decrypt ($_POST('parola_cryptata'), $ciphering,  "kalpsdnj", 0, $decryption_iv);
+$decryption=openssl_decrypt ($_POST('parola_criptata'), $ciphering, "kalpsdnj", 0, $encryption_iv);
+echo $decryption;}
 
 }
-
 ?>
         <html>
         <body>
         <FORM method="POST" action="decriptare.php">
           Email* :
           <INPUT TYPE="text" name="parola" required> <br>
-
+          <INPUT TYPE="text" name="parola_criptata" required> <br>
           <INPUT TYPE="submit" name="submit" VALUE="send">
        </form>
        </body>
