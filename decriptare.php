42d998946e033e1d611348ef60b04a0ffdba71df
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2022-12-10 00:51:26 +0200

    d

--- decriptare.php
+++ decriptare.php
@@ -1,4 +1,20 @@
+<?php
 
+echo $_POST('parola');
+$ciphering = "AES-128-CTR";
+$encryption_iv = '1234567891011121';
+$decryption_iv = '1234567891011121';
+
+//$encryption = openssl_encrypt($_POST('parola'), $ciphering,
+           // "kalpsdnj", 0, $encryption_iv);
+
+//echo $encryption;
+
+
+
+//$decryption=openssl_decrypt ($_POST('parola_cryptata'), $ciphering,  "kalpsdnj", 0, $decryption_iv);
+
+?>
         <html>
         <body>
         <FORM method="POST" action="decriptare.php">
