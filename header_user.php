c2ec2f301f78ba502d353681340ca3ed9fd766d1
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2022-12-12 00:07:06 +0200

    d

--- header_user.php
+++ header_user.php
@@ -29,7 +29,7 @@
               <li><hr class="dropdown-divider"></li>
               <li>
                 <?php echo "
-          <FORM style='height:32px;!important' method='POST' action='index.php'>
+          <FORM style='height:15px;!important' method='POST' action='index.php'>
           <INPUT class='dropdown-item' TYPE='submit' name='logout' VALUE='Logout'>
           </form>"; ?>
                     </li>
