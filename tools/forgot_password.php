7c243d0077c63df4afd42217f6898485cb54c487
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2022-12-21 00:53:17 +0200

    d

--- tools/forgot_password.php
+++ tools/forgot_password.php
@@ -9,25 +9,24 @@ if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   exit();
 }
-echo "ok1";
 $cerere_user="SELECT user_name,password FROM users WHERE email='".$_POST['email']." ' ";
-echo $cerere_user;
 $result_user= mysqli_query($conexiune, $cerere_user);
-
-echo "ok2";
 if (mysqli_num_rows($result_user) > 0){
 
   $row = mysqli_fetch_assoc($result_user);
 
   $subject="Recuperare Parola";
 
-  $body="Parola asociata contului ".$row["user_name"]."<br>
-  Este: ".openssl_decrypt($row['password'], "AES-128-CTR", "kalpsdnj", 0, '1234567891011121');
+  $body="Pentru contul cu
+  username: ".$row["user_name"]."<br>
+  email: ".$_POST['email']."<br>
+  parola este: ".openssl_decrypt($row['password'], "AES-128-CTR", "kalpsdnj", 0, '1234567891011121');
   $r_email=$_POST['email'];
   $r_user_name=$row['user_name'];
   mysqli_close($conexiune);
   include 'tools/mailer.php';
-  header('Location:index.php');
+  echo "Mesajul a fost trimis pe emailul asociat contului tau";
+
 }
 else echo "Nu a fost gasit un cont asociat cu acest email";
 mysqli_close($conexiune);
