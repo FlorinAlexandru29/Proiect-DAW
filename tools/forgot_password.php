e2c5c191a8946b06593ada7f9ae7c8e9e77a8c8e
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2022-12-21 00:42:25 +0200

    d

--- tools/forgot_password.php
+++ tools/forgot_password.php
@@ -1,13 +1,19 @@
 
 <?php
+
+
+if(isset($_POST['forgot_password'])){
   $conexiune=mysqli_connect('eu-cdbr-west-03.cleardb.net','bbd126d58cad2b','90feddf5','heroku_45e2f697954b823');
 
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   exit();
 }
+echo "ok1";
 $cerere_user="SELECT user_name,parola FROM users WHERE email='".$_POST['email']." ' ";
 $result_user= mysqli_query($conexiune, $cerere_user);
+
+echo "ok2";
 if (mysqli_num_rows($result_user) > 0){
 
   $row = mysqli_fetch_assoc($result_user);
@@ -20,10 +26,10 @@ if (mysqli_num_rows($result_user) > 0){
   $r_user_name=$row['user_name'];
   mysqli_close($conexiune);
   include 'tools/mailer.php';
-
+  header('Location:index.php');
 }
 else echo "Nu a fost gasit un cont asociat cu acest email";
 mysqli_close($conexiune);
-
+}
 
 ?>
