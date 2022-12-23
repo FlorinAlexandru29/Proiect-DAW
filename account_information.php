d203e3cf4275791cd621b44d724303e79f46cd97
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2022-12-24 01:10:25 +0200

    d

--- account_information.php
+++ account_information.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Home Page</title>
     <link href="resurse/bootstrap/bootstrap.css" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <script src="resurse/bootstrap/bootstrap.bundle.js"></script>
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
   </head>
 <?php 
   if (isset($_COOKIE["user_name"])) include 'header_user.php';
@@ -27,7 +29,7 @@ mysqli_close($conexiune);
     <ul class='list-group'>
     <li class='list-group-item'>
     <button type="button" class="btn btn-icon btn-light bg-white border rounded-circle shadow-sm position-absolute" data-bs-toggle="tooltip" data-bs-placement="bottom" aria-label="Change picture" style="width:36px;height:36px;">
-    <img style="width:18px;height:18px;" src="resurse/icons/upload_ico.png"></button>
+    <i class='bx bx-upload'></i>
 <?php
 
 if(isset($_COOKIE["profile_pic"])){
