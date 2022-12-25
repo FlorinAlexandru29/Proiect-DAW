7dbe30d031e3ed2c3d1f70bed7afb3144aa9d6c3
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2022-12-25 02:48:49 +0200

    d

---
+++ index1.php
@@ -0,0 +1,40 @@
+<?php
+
+if(isset($_POST['logout'])){     //scriptul de logout
+  setcookie("user_name", "guest", time()- 60,'/');
+  setcookie("profile_pic", '',time()-60,'/');
+  header('Location:index.php');
+}
+@session_start();
+if(isset($_SESSION['activat'])){
+echo "activeaza-ti contul";
+unset($_SESSION['activat']);
+}
+
+if(isset($_POST['submit'])) include 'login_v2.php';
+
+
+
+if(isset($_POST['forgot_password'])) include 'tools/forgot_password.php'
+
+
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home Page</title>
+    <link href="resurse/bootstrap/bootstrap.css" rel="stylesheet">
+    <script src="resurse/bootstrap/bootstrap.bundle.js"></script>
+  </head>
+  <?php 
+  if (isset($_COOKIE["user_name"])) include 'header_user.php';
+  else include 'header_guest.php'
+  ?>
+  <body>
+  
+  </body>
+</html>
+
+
