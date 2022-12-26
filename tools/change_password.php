00020e938a377ffaadd1d3f360a882786010a765
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2022-12-27 00:53:16 +0200

    d

--- tools/change_password.php
+++ tools/change_password.php
@@ -2,7 +2,7 @@
 
     if(isset($_POST['change_password'])){
         if(($_POST['password_s'])!=($_POST['password_c'])) echo "parole nu sunt la fel";
-
+        echo "ecmail".$row['email'];
         $cerere_password="SELECT password FROM users WHERE email='".$_row['email']." ' ";
         echo $cerere_password;
   $conexiune=mysqli_connect('eu-cdbr-west-03.cleardb.net','bbd126d58cad2b','90feddf5','heroku_45e2f697954b823');
