7a4aff35c8d542740f3f89d0ff3cac0126feda67
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2022-12-26 19:11:07 +0200

    d

--- poza.php
+++ poza.php
@@ -1,4 +1,4 @@
-<form action="upload.php" method="post" enctype="multipart/form-data">
+<form action="tools/upload.php" method="post" enctype="multipart/form-data">
 Select Image to upload:
 <input type="file" name="fileToUpload" id="fileToUpload">
 <input type="submit" value="Upload Image" name="submit">
