79abdafe52ddf4f6d8432b73147f0a127901a314
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2022-12-28 01:15:50 +0200

    d

--- google_upload.php
+++ google_upload.php
@@ -7,6 +7,12 @@ try {
     $storage = new StorageClient([
         'keyFilePath' => 'lure-prod-ee15fe45b34b.json',
     ]);
+    $bucketName = 'lure-prod-bucket';
+    $bucket = $storage->bucket($bucketName);
+    if (!$bucket->exists()) {
+        $response = $storage->createBucket($bucketName);
+        echo "Your Bucket $bucketName is created successfully.";
+    }
 } catch(Exception $e) {
     echo $e->getMessage();
 }
