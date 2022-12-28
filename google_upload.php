927b21eeb85f97c3408553ca1b5400e47b39bfd5
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2022-12-28 03:10:42 +0200

    d

--- google_upload.php
+++ google_upload.php
@@ -2,24 +2,6 @@
 require_once "vendor/autoload.php";
  
 use Google\Cloud\Storage\StorageClient;
- 
-$fileContent = file_get_contents($_FILES["file"]["tmp_name"]);
-$cloudPath = 'uploads/' . $_FILES["file"]["name"];
-
-$gs_name = $_FILES["uploaded_files"]["tmp_name"]; 
-$fileType = $_FILES["uploaded_files"]["type"]; 
-$fileSize = $_FILES["uploaded_files"]["size"]; 
-$fileErrorMsg = $_FILES["uploaded_files"]["error"]; 
-$fileExt = pathinfo($_FILES['uploaded_files']['name'], PATHINFO_EXTENSION);
-
-// put to cloud storage
-$image = file_get_contents($gs_name);
-$options = [ "gs" => [ "Content-Type" => "image/jpeg"]];
-$ctx = stream_context_create($options);
-file_put_contents("gs://<bucketname>/".$fileName, $gs_name, 0, $ctx);
-
-// or move 
-$moveResult = move_uploaded_file($gs_name, 'gs://<bucketname>/'.$fileName); 
 
 try {
     $storage = new StorageClient([
@@ -29,7 +11,7 @@ try {
 
     $bucketName = 'lure-prod-bucket';
     $fileName = '1.jpg';
-    
+
     $bucket = $storage->bucket($bucketName);
     $object = $bucket->upload(
         $file,
