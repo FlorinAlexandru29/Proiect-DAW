1a4a47d18d62bfabaa8af3462164b6c28eb8929f
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2022-12-28 03:17:56 +0200

    d

--- google_upload.php
+++ google_upload.php
@@ -7,20 +7,25 @@ try {
     $storage = new StorageClient([
         'keyFilePath' => 'lure-prod-ee15fe45b34b.json',
     ]);
-    $file = $_FILES["FileUpload1"]['tmp_name'];
+
+    $fileContent = file_get_contents($_FILES["file"]["tmp_name"]);
+    $cloudPath = 'uploads/' . $_FILES["file"]["name"];
+
 
     $bucketName = 'lure-prod-bucket';
     $fileName = '1.jpg';
 
     $bucket = $storage->bucket($bucketName);
-    $object = $bucket->upload(
-        $file,
-        [
-            'predefinedAcl' => 'publicRead'
-        ]
-    );
+    $storageObject = $bucket->upload(
+        $fileContent,
+        ['name' => $cloudPath]
+        // if $cloudPath is existed then will be overwrite without confirmation
+        // NOTE: 
+        // a. do not put prefix '/', '/' is a separate folder name  !!
+        // b. private key MUST have 'storage.objects.delete' permission if want to replace file !
+);
 
-    echo "File uploaded successfully. File path is: https://storage.googleapis.com/$bucketName/$file";
+    echo "File uploaded successfully. File path is: https://storage.googleapis.com/$bucketName/$cloudPath";
 } catch(Exception $e) {
     echo $e->getMessage();
 }
