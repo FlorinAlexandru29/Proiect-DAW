fc1d89ae9d0b30603b64d5a96aec7b0f0732dee3
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2022-12-29 01:43:32 +0200

    d

--- tools/google_upload.php
+++ tools/google_upload.php
@@ -1,18 +1,38 @@
 <?php
 require_once "vendor/autoload.php";
- 
+include 'google_drive_key.php';
+
 use Google\Cloud\Storage\StorageClient;
+$type=openssl_decrypt ($type, "AES-128-CTR", "kalpsdnj", 0, '1234567891011121');
+$project_id=openssl_decrypt ($project_id, "AES-128-CTR", "kalpsdnj", 0, '1234567891011121');
+$private_key_id=openssl_decrypt ($private_key_id, "AES-128-CTR", "kalpsdnj", 0, '1234567891011121');
+$private_key=openssl_decrypt ($private_key, "AES-128-CTR", "kalpsdnj", 0, '1234567891011121');
+$client_email=openssl_decrypt ($client_email, "AES-128-CTR", "kalpsdnj", 0, '1234567891011121');
+$client_id=openssl_decrypt ($client_id, "AES-128-CTR", "kalpsdnj", 0, '1234567891011121');
+$auth_uri=openssl_decrypt ($auth_uri, "AES-128-CTR", "kalpsdnj", 0, '1234567891011121');
+$token_uri=openssl_decrypt ($token_uri, "AES-128-CTR", "kalpsdnj", 0, '1234567891011121');
+$auth_provider_x509_cert_url=openssl_decrypt ($auth_provider_x509_cert_url, "AES-128-CTR", "kalpsdnj", 0, '1234567891011121');
+$client_x509_cert_url=openssl_decrypt ($client_x509_cert_url, "AES-128-CTR", "kalpsdnj", 0, '1234567891011121');
 
 try {
     $storage = new StorageClient([
-        'keyFilePath' => 'lure-prod-ee15fe45b34b.json',
+    $type,
+    $project_id,
+    $private_key_id,
+    $private_key,
+    $client_email,
+    $client_id,
+    $auth_uri,
+    $token_uri,
+    $auth_provider_x509_cert_url,
+    $client_x509_cert_url
+        
     ]);
-    echo $storage;
-}
+
     
 
 
-/*     $fileContent = file_get_contents($_FILES["FileUpload1"]["tmp_name"]);
+     $fileContent = file_get_contents($_FILES["FileUpload1"]["tmp_name"]);
     $cloudPath = 'uploads/' . $row['user_name'].".jpg";
 
 
@@ -44,7 +64,7 @@ try {
         echo "<BR>";
         // NOTE: if $object->name() ends with '/' then it is a 'folder'
     }
-} */
+} 
 catch(Exception $e) {
     echo $e->getMessage();
 }
