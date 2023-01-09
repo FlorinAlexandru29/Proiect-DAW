d2759ba190a554966867d8178c91a975befbbecd
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2023-01-10 00:50:44 +0200

    d

--- trupa.php
+++ trupa.php
@@ -8,10 +8,8 @@ if(isset($_GET["id"]))
     exit();
   }
   $trupa=str_replace("+"," ",$_GET["id"]);
-  echo $trupa;
  
   $cerere_trupa="SELECT * FROM trupe WHERE nume='".$trupa." ' ";
-  echo $cerere_trupa;
  
   $result_trupa= mysqli_query($conexiune, $cerere_trupa);
   $row_trupa = mysqli_fetch_assoc($result_trupa);
@@ -102,13 +100,6 @@ else include 'fragmente/navbar_guest.php'
                 </button>
                 </div>
         <?php 
-    /*     echo "<p>".$result_trupa["nume"]."</p>
-        <div class='d-flex justify-content-between'>
-        <p> Oras: ".$result_trupa['oras']."</p>
-        <p> Gen: ".$result_trupa['gen']."</p>
-        <p> An Infiintare: ".$result_trupa['year']."</p>
-        </div>
-        " */
         ?>
       </div>
       </div>
