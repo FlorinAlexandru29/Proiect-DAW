f8d371fe7e0faf2b96136422e49b5671af81a591
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2023-01-12 00:55:14 +0200

    d

--- postare.php
+++ postare.php
@@ -110,6 +110,226 @@ if(isset($_GET["id"]))
             allowfullscreen></iframe>
             ";}
             ?>
-            
+            <hr class="my-4 w-100">
+            <section class="gradient-custom w-100">
+            <div class="container">
+              <div class="row d-flex justify-content-center">
+                <div class="col p-0">
+                  <div class="card">
+                    <div class="card-body p-4">
+                      <h4 class="text-center mb-4 pb-2">Nested comments section</h4>
+          
+                      <div class="row">
+                        <div class="col">
+<!--inceput casuta comentariu-->
+<div class="row mb-5">
+  <div class="d-flex flex-start"  >
+    
+      <img class="me-3 rounded-circle shadow-1-strong"
+        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp" alt="avatar"
+        width="65" height="65" />
+    
+    <div class="flex-grow-1 flex-shrink-1">
+      
+        <div class="d-flex align-items-start">
+          <p class="mb-1">
+            User_Name
+          </p>
+        </div>
+        <form method="POST" action="tools/adaugare-comentariu.php" id="form-adaugare-comentariu">
+        <textarea class='form-control' name="comm" rows="5" style="resize: none;" required> </textarea>
+      </div>
+    </div>
+    <div class="d-flex col justify-content-end mt-2">
+      <button type="submit" form="form-adaugare-comentariu" name="add_comm" class="d-flex align-items-center btn btn-primary rounded-pill"><span class="small">Send</span><i class='bx bxs-send fs-lg ms-2'></i></button>
+    </form>
+    </div>
+    </div>
+<!--sf casuta comentariu-->
+                          <div class="d-flex flex-start">
+                            <img class="rounded-circle shadow-1-strong me-3"
+                              src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(10).webp" alt="avatar" width="65"
+                              height="65" />
+                            <div class="flex-grow-1 flex-shrink-1">
+                              <div>
+                                <div class="d-flex justify-content-between align-items-center">
+                                  <p class="mb-1">
+                                    Maria Smantha <span class="small">- 2 hours ago</span>
+                                  </p>
+                                  <button class="d-flex align-items-center btn btn-outline-primary rounded-pill btn-sm" onclick="show_reply(1)"><i class="bx bx-share fs-lg me-2"></i><span class="small"> reply</span></button>
+                                </div>
+                                <p class="small mb-0">
+                                  It is a long established fact that a reader will be distracted by
+                                  the readable content of a page.
+                                </p>
+                              </div>
+          
+                              <div class="d-flex flex-start mt-4">
+                                <a class="me-3" href="#">
+                                  <img class="rounded-circle shadow-1-strong"
+                                    src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(11).webp" alt="avatar"
+                                    width="65" height="65" />
+                                </a>
+                                <div class="flex-grow-1 flex-shrink-1">
+                                  <div>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                      <p class="mb-1">
+                                        Simona Disa <span class="small">- 3 hours ago</span>
+                                      </p>
+                                    </div>
+                                    <p class="small mb-0">
+                                      letters, as opposed to using 'Content here, content here',
+                                      making it look like readable English.
+                                    </p>
+                                  </div>
+                                </div>
+                              </div>
+          
+                              <div class="d-flex flex-start mt-4">
+                                <a class="me-3" href="#">
+                                  <img class="rounded-circle shadow-1-strong"
+                                    src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp" alt="avatar"
+                                    width="65" height="65" />
+                                </a>
+                                <div class="flex-grow-1 flex-shrink-1">
+                                  <div>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                      <p class="mb-1">
+                                        John Smith <span class="small">- 4 hours ago</span>
+                                      </p>
+                                    </div>
+                                    <p class="small mb-0">
+                                      the majority have suffered alteration in some form, by
+                                      injected humour, or randomised words.
+                                    </p>
+                                  </div>
+                                </div>
+                              </div>
+                              <!-- ultimul comentariu-->
+                              <div class="row" id="reply-comm1" style="display:none !important;"  >
+                              <div class="d-flex flex-start mt-4"  >
+                                <a class="me-3" href="#">
+                                  <img class="rounded-circle shadow-1-strong"
+                                    src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp" alt="avatar"
+                                    width="65" height="65" />
+                                </a>
+                                <div class="flex-grow-1 flex-shrink-1">
+                                  <div>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                      <p class="mb-1">
+                                        John Smith
+                                      </p>
+                                    </div>
+                                    <textarea class='form-control' id="descriere" name="descriere" rows="5" style="resize: none;" required> </textarea>
+                                  </div>
+                                </div>
+                              </div>
+                                <div class="d-flex col justify-content-end mt-2">
+                                  <button class="d-flex align-items-center btn btn-outline-danger rounded-pill" style="margin-left:65px;" onclick="hide_reply(1)"><span class="small">Cancel</span><i class='bx bx-message-x fs-lg ms-2'></i></button>
+                                  <button class="ms-5 d-flex align-items-center btn btn-primary rounded-pill"><span class="small">Send</span><i class='bx bxs-send fs-lg ms-2'></i></button>
+                                  
+                                </div>
+
+                                </div>
+                              
+
+                              <!-- form reply-->
+                            </div>
+                          </div>
+          
+         
+                          <div class="d-flex flex-start mt-4">
+                            <img class="rounded-circle shadow-1-strong me-3"
+                              src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(12).webp" alt="avatar" width="65"
+                              height="65" />
+                            <div class="flex-grow-1 flex-shrink-1">
+                              <div>
+                                <div class="d-flex justify-content-between align-items-center">
+                                  <p class="mb-1">
+                                    Natalie Smith <span class="small">- 2 hours ago</span>
+                                  </p>
+                                  <a href="#!"><i class="fas fa-reply fa-xs"></i><span class="small"> reply</span></a>
+                                </div>
+                                <p class="small mb-0">
+                                  The standard chunk of Lorem Ipsum used since the 1500s is
+                                  reproduced below for those interested. Sections 1.10.32 and
+                                  1.10.33.
+                                </p>
+                              </div>
+          
+                              <div class="d-flex flex-start mt-4">
+                                <a class="me-3" href="#">
+                                  <img class="rounded-circle shadow-1-strong"
+                                    src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(31).webp" alt="avatar"
+                                    width="65" height="65" />
+                                </a>
+                                <div class="flex-grow-1 flex-shrink-1">
+                                  <div>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                      <p class="mb-1">
+                                        Lisa Cudrow <span class="small">- 4 hours ago</span>
+                                      </p>
+                                    </div>
+                                    <p class="small mb-0">
+                                      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+                                      scelerisque ante sollicitudin commodo. Cras purus odio,
+                                      vestibulum in vulputate at, tempus viverra turpis.
+                                    </p>
+                                  </div>
+                                </div>
+                              </div>
+          
+                              <div class="d-flex flex-start mt-4">
+                                <a class="me-3" href="#">
+                                  <img class="rounded-circle shadow-1-strong"
+                                    src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(29).webp" alt="avatar"
+                                    width="65" height="65" />
+                                </a>
+                                <div class="flex-grow-1 flex-shrink-1">
+                                  <div>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                      <p class="mb-1">
+                                        Maggie McLoan <span class="small">- 5 hours ago</span>
+                                      </p>
+                                    </div>
+                                    <p class="small mb-0">
+                                      a Latin professor at Hampden-Sydney College in Virginia,
+                                      looked up one of the more obscure Latin words, consectetur
+                                    </p>
+                                  </div>
+                                </div>
+                              </div>
+          
+                              <div class="d-flex flex-start mt-4">
+                                <a class="me-3" href="#">
+                                  <img class="rounded-circle shadow-1-strong"
+                                    src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp" alt="avatar"
+                                    width="65" height="65" />
+                                </a>
+                                <div class="flex-grow-1 flex-shrink-1">
+                                  <div>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                      <p class="mb-1">
+                                        John Smith <span class="small">- 6 hours ago</span>
+                                      </p>
+                                    </div>
+                                    <p class="small mb-0">
+                                      Autem, totam debitis suscipit saepe sapiente magnam officiis
+                                      quaerat necessitatibus odio assumenda, perferendis quae iusto
+                                      labore laboriosam minima numquam impedit quam dolorem!
+                                    </p>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
             </div>
             </main>
