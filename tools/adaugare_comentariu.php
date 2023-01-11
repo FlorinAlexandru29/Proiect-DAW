ce449c1ab1bcfece06718364fb194fc9807a83d7
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2023-01-12 01:24:41 +0200

    d

--- tools/adaugare_comentariu.php
+++ tools/adaugare_comentariu.php
@@ -1,7 +1,8 @@
 <?php
 if (isset($_POST['add_comm']))
-{
-    $cerere="INSERT INTO comments (id_comment,id_postare,user_name,data_comentariu,comentariu,reply) values('".uniqid()."','".$_POST['post_id']."'";
+{   date_default_timezone_set('Europe/Bucharest');
+    $data_comentariu = date('Y-m-d H:i');
+    $cerere="INSERT INTO comments (id_comment,id_postare,user_name,data_comentariu,comentariu,reply) values('".uniqid()."','".$_POST['post_id']."','".openssl_decrypt ($_COOKIE["user_name"], "AES-128-CTR", "kalpsdnj", 0, '1234567891011121')."','".$data_comentariu."',";
     echo $cerere;
 }
 
