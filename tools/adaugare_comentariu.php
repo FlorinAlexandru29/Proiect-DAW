3d1391a29ba9d92f01de641cc9deaeeb19a0c8b1
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2023-01-12 15:42:19 +0200

    d

--- tools/adaugare_comentariu.php
+++ tools/adaugare_comentariu.php
@@ -12,7 +12,7 @@ if (isset($_POST['add_comm']))
     $cerere="INSERT INTO comments (id_comment,id_postare,user_name,data_comentariu,comentariu,reply) values('".uniqid()."','".$_POST['post_id']."','".openssl_decrypt ($_COOKIE["user_name"], "AES-128-CTR", "kalpsdnj", 0, '1234567891011121')."','".$data_comentariu."','".$_POST['comm']."','".$_POST['reply']."')";
      mysqli_query($conexiune, $cerere);
     mysqli_close($conexiune);  
-    $header="Location:postare.php?id=".$_POST['post_id'];
+    $header="Location:../postare.php?id=".$_POST['post_id'];
     header($header);
     exit();
 }
