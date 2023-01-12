aa0151cf666d0c15b07d8249d4ad67960594a304
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2023-01-12 15:37:37 +0200

    d

--- tools/adaugare_comentariu.php
+++ tools/adaugare_comentariu.php
@@ -8,6 +8,7 @@ if (isset($_POST['add_comm']))
 
     date_default_timezone_set('Europe/Bucharest');
     $data_comentariu = date('Y-m-d H:i');
+    //problema -> daca userul se deconecteaza (i.e ii expira cookie-ul, o sa dea fail insertul)
     $cerere="INSERT INTO comments (id_comment,id_postare,user_name,data_comentariu,comentariu,reply) values('".uniqid()."','".$_POST['post_id']."','".openssl_decrypt ($_COOKIE["user_name"], "AES-128-CTR", "kalpsdnj", 0, '1234567891011121')."','".$data_comentariu."','".$_POST['comm']."','".$_POST['reply']."')";
      mysqli_query($conexiune, $cerere);
     mysqli_close($conexiune);  
