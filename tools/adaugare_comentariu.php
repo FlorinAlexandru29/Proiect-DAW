1443a427c434ceb47fb85c1ca55bed32cafa491f
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2023-01-12 15:36:17 +0200

    d

--- tools/adaugare_comentariu.php
+++ tools/adaugare_comentariu.php
@@ -1,9 +1,19 @@
 <?php
 if (isset($_POST['add_comm']))
-{   date_default_timezone_set('Europe/Bucharest');
+{   $conexiune=mysqli_connect('eu-cdbr-west-03.cleardb.net','bbd126d58cad2b','90feddf5','heroku_45e2f697954b823');
+    if (mysqli_connect_errno()) {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+      exit();
+    }
+
+    date_default_timezone_set('Europe/Bucharest');
     $data_comentariu = date('Y-m-d H:i');
     $cerere="INSERT INTO comments (id_comment,id_postare,user_name,data_comentariu,comentariu,reply) values('".uniqid()."','".$_POST['post_id']."','".openssl_decrypt ($_COOKIE["user_name"], "AES-128-CTR", "kalpsdnj", 0, '1234567891011121')."','".$data_comentariu."','".$_POST['comm']."','".$_POST['reply']."')";
-    echo $cerere;
+     mysqli_query($conexiune, $cerere);
+    mysqli_close($conexiune);  
+    $header="Location:postare.php?id=".$_POST['post_id'];
+    header($header);
+    exit();
 }
 
 ?>
