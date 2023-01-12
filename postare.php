702954e2ec2ab8cff993d3c375c5e9beb7075bf0
Anghelescu Florin <119753268+FlorinAlexandru29@users.noreply.github.com>
2023-01-12 02:30:06 +0200

    d

--- postare.php
+++ postare.php
@@ -185,7 +185,7 @@ if (mysqli_num_rows($result_comentariu) > 0){
           <p class='mb-1'>
             ".$row_comentariu['user_name']." <span class='small'>- ".$row_comentariu['data_comentariu']."</span>
           </p>
-          <button class='d-flex align-items-center btn btn-outline-primary rounded-pill btn-sm' onclick='show_reply('".$row_comentariu['id_comment']."')'><i class='bx bx-share fs-lg me-2'></i><span class='small'> reply</span></button>
+          <button class='d-flex align-items-center btn btn-outline-primary rounded-pill btn-sm' onclick='show_reply(".$row_comentariu['id_comment'].")'><i class='bx bx-share fs-lg me-2'></i><span class='small'> reply</span></button>
         </div>
         <p class='small mb-0'>
          ".$row_comentariu['comentariu']."
