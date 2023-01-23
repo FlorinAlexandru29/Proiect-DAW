94bfd03e80a479cf2753052cf12af59b428891d8
Anghelescu Florin <florin-alexandru.anghelescu@s.unibuc.ro>
2023-01-23 15:54:11 +0200

    d

--- test_api.php
+++ test_api.php
@@ -5,5 +5,13 @@ curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $data = curl_exec($ch);
 curl_close($ch);
-echo $data;
+
+$dom = new DOMDocument;
+$dom->loadHTML($data);
+$xpath = new DOMXPath($dom);
+
+$elements = $xpath->query("//div[@class='date-start']");
+foreach ($elements as $element) {
+    echo $element->nodeValue;
+}
 ?>
