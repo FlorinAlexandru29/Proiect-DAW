8c58d6fa2ef98c5c787126cd3837ccfb8782960c
Anghelescu Florin <florin-alexandru.anghelescu@s.unibuc.ro>
2023-01-30 13:18:19 +0200

    d

--- test_api.php
+++ test_api.php
@@ -34,8 +34,11 @@ for($i=0;$i<$n;$i++){
 $linkuri = $xpath->query("//div[@class='col-xs-2']");
 $i=0;
 foreach($linkuri as $link){
-
     $link_concert[$i]=$link->nodeValue;
+    if ((empty ($link_concert[$i])) && (empty ($link_concert[$i-1])))
+    echo "Sold Out";
+
+
     echo "link_concert nr".$i.$link_concert[$i]."<br>";
     $i=$i+1;
 }
