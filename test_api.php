b66bc51791b3be569572aa2e84c9d1fcefd73483
Anghelescu Florin <florin-alexandru.anghelescu@s.unibuc.ro>
2023-01-31 22:54:51 +0200

    d

--- test_api.php
+++ test_api.php
@@ -1,5 +1,5 @@
 <?php
-$url = "https://www.iabilet.ro/cauta/?q=trooper";
+$url = "https://www.iabilet.ro/cauta/?q=bucovina";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -26,27 +26,59 @@ foreach ($titluri as $titlu) {
     $i=$i+1;
 }
 $n=$i;
-for($i=0;$i<$n;$i++){
-    echo $data_concert[$i] ."<br>" . $titlu_concert[$i] . "<br>";
-}
+
 
 
 $linkuri = $xpath->query("//div[@class='col-xs-2']");
-$i=0;
+$i=0;$j=0;
 foreach($linkuri as $link){
     $link_concert[$i]=$link->nodeValue;
-    if ($i>0){
-    if (( str_contains($link_concert[$i],"ia bilet")==FALSE && str_contains($link_concert[$i-1],"ia bilet")==FALSE))
-    echo "Sold Out<br>";}
 
+    if ($i>1){
+    if (( str_contains($link_concert[$i],"ia bilet")==FALSE && str_contains($link_concert[$i-1],"ia bilet")==FALSE)){
+    $pret_concert[$j]="Sold Out";
+    $j++; }
+    else if (str_contains($link_concert[$i],"ia bilet")==TRUE) {$pret_concert[$j]=$link_concert[$i];$j++;
+    }
 
-   if (str_contains($link_concert[$i],"ia bilet")==TRUE) echo "link_concert nr".$i.$link_concert[$i]."<br>";
-    $i=$i+1;
+   
+    }
+    if ($i==0) {
+      
+         $pret_concert[$j]="Sold Out";$j++;
+        
+    }
+    $i++;
+    
+
+}   
+$i=0;$z=0;
+foreach($linkuri as $link){
+    $a_tags = $link->getElementsByTagName('a');
+    if ($a_tags->length > 0) {
+        $a_tag = $a_tags->item(0);
+        $href[$i] = $a_tag->getAttribute("href");
+        if ($i>0){
+        if ($href[$i]!=$href[$i-1]){
+            $href_concert[$z]=$href[$i];
+            $z++;
+        }
+        }
+        else {$href_concert[$z]=$href[$i];
+            $z++;}
+
+        $i=$i+1;
+    }
 }
 
-$links = $xpath->query("//a");
-foreach ($links as $link) {
-    $href = $link->getAttribute("href");
-    echo $href . "<br>";
+$m=$j;
+$linkuri = $xpath->query("//div[@class='col-xs-2']");
+$i=0;
+
+    
+
+
+for($i=0;$i<$n;$i++){
+    echo $data_concert[$i] ."<br>" . $titlu_concert[$i] . "<br>" . $pret_concert[$i] . "<br>".$href_concert[$i] . "<br><br>";
 }
 ?>
