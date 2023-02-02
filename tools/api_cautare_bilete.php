b1428785444674c68e6740a09680615b234a287b
Anghelescu Florin <florin-alexandru.anghelescu@s.unibuc.ro>
2023-02-02 23:26:48 +0200

    d

--- tools/api_cautare_bilete.php
+++ tools/api_cautare_bilete.php
@@ -1,5 +1,5 @@
 <?php
-$url = "https://www.iabilet.ro/cauta/?q=".$_GET["id"]."";
+$url = "https://www.iabilet.ro/cauta/?q=".$id_trupa."";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
