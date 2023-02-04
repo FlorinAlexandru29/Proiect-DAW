40a193b8c05d0d3199dc1f644aec0d0431f365c5
Anghelescu Florin <florin-alexandru.anghelescu@s.unibuc.ro>
2023-02-04 08:35:41 +0200

    d

--- tools/user_management.php
+++ tools/user_management.php
@@ -15,8 +15,8 @@ if(isset($_POST['logout'])){     //scriptul de logout
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>User Management
     </title>
-    <link href="resurse/bootstrap/bootstrap.css" rel="stylesheet">
-    <script src="resurse/bootstrap/bootstrap.bundle.js"></script>
+    <link href="../resurse/bootstrap/bootstrap.css" rel="stylesheet">
+    <script src="../resurse/bootstrap/bootstrap.bundle.js"></script>
     <style> @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap'); </style>
     <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
