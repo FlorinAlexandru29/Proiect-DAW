57b72f5cadf0d01e8bc3be90ccdbc65dd19a90dc
Anghelescu Florin <florin-alexandru.anghelescu@s.unibuc.ro>
2023-02-05 02:33:54 +0200

    d

--- trupa.php
+++ trupa.php
@@ -255,13 +255,14 @@ else {$pagina_request_login="trupa.php?id=".$_GET["id"];
     <h2 class="accordion-header" id="headingThree">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
         <?php 
+        $id_trupa=$_GET["id"];
+        echo $id_trupa;
         echo "Concerte ".$row_trupa['nume']."
           </button>
           </h2>
           <div id='collapseThree' class='accordion-collapse collapse' aria-labelledby='headingThree' data-bs-parent='#accordionExample'>
           ";
-          $id_trupa=($_GET["id"]);
-          echo $id_trupa;
+          
           //include 'tools/api_cautare_bilete.php';
           if (isset($eroare_cautare))
           {
